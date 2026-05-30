1b69dad86aea6f401f7ebec15648e0352bd5c3ca
NuhaOnly <ahunnilurajaf@gmail.com>
2026-05-30 10:50:26 +0700

    Remove Mapbox token

--- public/frontend/js/map.js
+++ public/frontend/js/map.js
@@ -3,7 +3,9 @@ var mymap = L.map('map', {
     zoom: 13
 });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+const mapboxToken = '';
+
+L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${mapboxToken}`, {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
